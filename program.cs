2debe842ea3acbff99558584219bce6ec089e188
dEnsssy <kravchuk.n.o@gmail.com>
2023-06-20 16:17:10 +0300

    Create Method PrintArray

--- program.cs
+++ program.cs
@@ -7,7 +7,18 @@
    }
    return array;
 }
+void PrintArray(string[] array)
+{
+   for (int i = 0; i < array.Length - 1; i++)
+   {
+      Console.Write($"\"{array[i]}\", ");
+   }
+   Console.Write($"\"{array[array.Length - 1]}\"");
+}
 
 Console.Write("Введите длинну массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 string[] array = FillArray(n);
+Console.Write("[");
+PrintArray(array);
+Console.Write("] →");
