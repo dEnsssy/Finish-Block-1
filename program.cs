01a9833dd4e10f9175f56fb936a2a043028caf8e
dEnsssy <kravchuk.n.o@gmail.com>
2023-06-20 16:16:01 +0300

    Create Method FillArray

--- program.cs
+++ program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] FillArray(int size)
+{
+   string[] array = new string[size];
+   for (int i = 0; i < array.Length; i++)
+   {
+      array[i] = Console.ReadLine();
+   }
+   return array;
+}
+
+Console.Write("Введите длинну массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = FillArray(n);
